edae3ed135aba64c8bdea9face8b65dfdd1a5e99
rajibsutradhartonmoy <rajibsutradhartonmoy@gmail.com>
2026-08-11 12:08:29 +0600

    feat(proxy): update matcher to exclude 'openapi.json' from routing

--- apps/publicator/src/proxy.ts
+++ apps/publicator/src/proxy.ts
@@ -34,6 +34,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|backend|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|backend|openapi.json|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
